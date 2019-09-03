c4fbfa30f2ec753b084b09e33991450be3b9e7e2
Roula-Alhaddad <roulahd85@gmail.com>
2019-09-03 13:20:54 +0200

    Code changes regarding camelCasing naming conventions

--- server.js
+++ server.js
@@ -4,44 +4,29 @@ const express = require('express');
 const app = express();
 const {Datastore} = require('@google-cloud/datastore');
 const datastore = new Datastore();
+const PORT = process.env.PORT || 8080;
+const ENTITY ='IkeaCustomers';
 
-
-
-
-//************ Express Routes here **************//
-//Default Route
 app.get('/', (req, res) => {
-  res.send('Hello from App Engine!');
+  res.send('Ikea Customers');
 });
-
-
 //getCustomers 
-app.get('/getcustomers', async function(req, res) {
-    const query = datastore.createQuery('IkeaCustomers')
+app.get('/getCustomers', async function(req, res) {
+    const query = datastore.createQuery(ENTITY);
     const customers = await datastore.runQuery(query);
     res.send(customers).end();
 });
-
-
 //getCustomersbyid
-app.get('/getCustomersbyid/:Id', async function(req, res)  {
-      const query = datastore.createQuery('IkeaCustomers').filter('Id', req.params.Id ); 
+app.get('/getCustomer/:id', async function(req, res)  {	
+    const customerId = Number(req.params.id);
+    const query = datastore.createQuery(ENTITY).filter('id', customerId ); 
     const customer = await datastore.runQuery(query);
     res.send(customer).end(); 
 });
-
-
-//Catch all other routes
 app.get('*', (req, res) => {
   res.send('Invalid Page');
 });
-
-
-// Listen to the App Engine-specified port, or 8080 otherwise
-const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
-
-
 module.exports = app;
